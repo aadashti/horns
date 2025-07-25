b58155bafa2bc7394a23131f96cb0b6b481211e6
aadashti <devoft1@gmail.com>
2025-07-25 23:17:37 +0000

    modified:   src/modules/install.rs modified:   src/modules/script.rs

--- src/modules/install.rs
+++ src/modules/install.rs
@@ -11,8 +11,8 @@ fn run(cmd: &str) -> bool {
 
 pub fn install_tool(title: &str, package_string: &str, ask: &str) -> bool {
     let package_list: Vec<&str> = package_string.split_whitespace().collect();
-    // println!("Packages to install: {:?}", package_list);
-    // println!("Ask prompt: {}", ask);
+    println!("Packages to install: {:?}", package_list);
+    println!("Ask prompt: {}", ask);
 
     for package in package_list {
         let tag = format!("\x1B[32m{}\x1B[0m", format!("[Installing {} >> {}]", package, title));
